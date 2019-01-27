3dc394c7536dfe5ef04d23dc7f8c09e312f60174
Pieter Nijs <pieternijs@live.be>
2019-01-27 10:19:36 +0100

    Updated UWP Bluetooth Communication

--- Lego.Ev3.UWP/BluetoothCommunication.cs
+++ Lego.Ev3.UWP/BluetoothCommunication.cs
@@ -1,11 +1,14 @@
 ﻿using Lego.Ev3.Core;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.Bluetooth.Rfcomm;
 using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Networking.Sockets;
 using Windows.Storage.Streams;
@@ -55,28 +58,69 @@ namespace Lego.Ev3.UWP
 			return ConnectAsyncInternal();
 		}
 
-		private async Task ConnectAsyncInternal()
+        public async Task<string> ComPort(DeviceInformation deviceInfo)
+        {
+            var serialDevices = new Dictionary<string, SerialDevice>();
+            var serialSelector = SerialDevice.GetDeviceSelector();
+            var serialDeviceInformations = (await DeviceInformation.FindAllAsync(serialSelector)).ToList();
+            var hostNames = Windows.Networking.Connectivity.NetworkInformation.GetHostNames().Select(hostName => hostName.DisplayName.ToUpper()).ToList(); // So we can ignore inbuilt ports
+            foreach (var serialDeviceInformation in serialDeviceInformations)
+            {
+                if (hostNames.FirstOrDefault(hostName => hostName.StartsWith(serialDeviceInformation.Name.ToUpper())) == null)
+                {
+                    try
+                    {
+                        var serialDevice = await SerialDevice.FromIdAsync(serialDeviceInformation.Id);
+                        if (serialDevice != null)
+                        {
+                            serialDevices.Add(deviceInfo.Id, serialDevice);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    }
+                }
+            }
+            // Example Bluetooth DeviceInfo.Id: "Bluetooth#Bluetooth9c:b6:d0:d6:d7:56-00:07:80:cb:56:6d"
+            // from device with Association Endpoint Address: "00:07:80:cb:56:6d"
+            var lengthOfTrailingAssociationEndpointAddresss = (2 * 6) + 5;
+            var bluetoothDeviceAddress = deviceInfo.Id.Substring(deviceInfo.Id.Length - lengthOfTrailingAssociationEndpointAddresss, lengthOfTrailingAssociationEndpointAddresss).Replace(":", "").ToUpper();
+            var matchingKey = serialDevices.Keys.FirstOrDefault(id => id.Contains(bluetoothDeviceAddress));
+            if (matchingKey != null)
+            {
+                return serialDevices[matchingKey].PortName;
+            }
+            return "";
+        }
+
+        private async Task ConnectAsyncInternal()
 		{
 			_tokenSource = new CancellationTokenSource();
 
-			string selector = RfcommDeviceService.GetDeviceSelector(RfcommServiceId.SerialPort);
-			DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
-			DeviceInformation device = (from d in devices where d.Name == _deviceName select d).FirstOrDefault();
-			if(device == null)
-				throw new Exception("LEGO EV3 brick named '" + _deviceName + "' not found.");
+            var qry = "System.Devices.InterfaceClassGuid:=\"{b142fc3e-fa4e-460b-8abc-072b628b3c70}\"";
 
-			RfcommDeviceService service = await RfcommDeviceService.FromIdAsync(device.Id);
-			if(service == null)
-				throw new Exception("Unable to connect to LEGO EV3 brick...is the manifest set properly?");
+            var all = await DeviceInformation.FindAllAsync(qry);
+            var device = all.FirstOrDefault(aa => aa.Name == _deviceName);
 
-			_socket = new StreamSocket();
-			await _socket.ConnectAsync(service.ConnectionHostName, service.ConnectionServiceName,
-				 SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
 
-			_reader = new DataReader(_socket.InputStream);
-			_reader.ByteOrder = ByteOrder.LittleEndian;
+            if (device == null)
+                throw new Exception("LEGO EV3 brick named '" + _deviceName + "' not found.");
 
-			await ThreadPool.RunAsync(PollInput);
+            RfcommDeviceService service = await RfcommDeviceService.FromIdAsync(device.Id);
+            if (service == null)
+                throw new Exception("Unable to connect to LEGO EV3 brick...is the manifest set properly?");
+
+            var a = device.Properties;
+
+            _socket = new StreamSocket();
+            await _socket.ConnectAsync(service.ConnectionHostName, service.ConnectionServiceName,
+                 SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
+
+            _reader = new DataReader(_socket.InputStream);
+            _reader.ByteOrder = ByteOrder.LittleEndian;
+
+            await ThreadPool.RunAsync(PollInput);		
 		}
 
 		private async void PollInput(IAsyncAction operation)
